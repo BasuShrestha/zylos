81b58bcbe5ed5774296f64705b68f160cf71504f
Basu Shrestha <basu.stha4@gmail.com>
2026-06-08 10:59:59 +0545

    refactored player_provider to use StateNotifierProvider to NotifierProvider

--- lib/providers/player_provider.dart
+++ lib/providers/player_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import 'package:just_audio/just_audio.dart';
 
 import '../audio/audio_handler.dart';
@@ -10,15 +9,22 @@ final audioHandlerProvider = Provider<ZylosAudioHandler>((ref) {
   throw UnimplementedError('Must be overridden in main.dart');
 });
 
-final playerProvider = StateNotifierProvider<PlayerNotifier, PlayerStateModel>(
-  (ref) => PlayerNotifier(ref.watch(audioHandlerProvider)),
+final playerProvider = NotifierProvider<PlayerNotifier, PlayerStateModel>(
+  PlayerNotifier.new,
 );
 
-class PlayerNotifier extends StateNotifier<PlayerStateModel> {
-  final ZylosAudioHandler _handler;
+class PlayerNotifier extends Notifier<PlayerStateModel> {
+  late final ZylosAudioHandler _handler;
 
-  PlayerNotifier(this._handler) : super(const PlayerStateModel()) {
+  // PlayerNotifier(this._handler) : super(const PlayerStateModel()) {
+  //   _listenToPlayer();
+  // }
+
+  @override
+  PlayerStateModel build() {
+    _handler = ref.watch(audioHandlerProvider);
     _listenToPlayer();
+    return const PlayerStateModel();
   }
 
   void _listenToPlayer() {
@@ -54,9 +60,8 @@ class PlayerNotifier extends StateNotifier<PlayerStateModel> {
   }
 
   Future<void> playSong(List<SongModel> queue, int index) async {
-    final song = queue[index];
     state = state.copyWith(
-      currentSong: song,
+      currentSong: queue[index],
       queue: queue,
       currentIndex: index,
       position: Duration.zero,
@@ -65,16 +70,12 @@ class PlayerNotifier extends StateNotifier<PlayerStateModel> {
   }
 
   Future<void> togglePlayPause() async {
-    if (state.isPlaying) {
-      await _handler.pause();
-    } else {
-      await _handler.play();
-    }
+    state.isPlaying ? await _handler.pause() : await _handler.play();
   }
 
   Future<void> next() async {
     if (!state.hasNext) return;
-    await playSong(state.queue, state.currentIndex + 1);
+    await _handler.seekToNext();
   }
 
   Future<void> previous() async {
@@ -83,7 +84,7 @@ class PlayerNotifier extends StateNotifier<PlayerStateModel> {
       return;
     }
     if (!state.hasPrevious) return;
-    await playSong(state.queue, state.currentIndex - 1);
+    await _handler.seekToPrevious();
   }
 
   Future<void> seekTo(Duration position) => _handler.seek(position);
